084b899e3beb39ef2fe26f301971dcae14cd5114
DevArslan <arslan.gareev.2017@gmail.com>
2021-03-22 00:29:32 +0300

    master fix deploy

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/dist/CASE-IN'));
+app.use(express.static(__dirname + '/dist/case-in'));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/CASE-IN/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/case-in/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
